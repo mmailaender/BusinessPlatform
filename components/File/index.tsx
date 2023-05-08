8871dd19c400fca99c791875b07ad31eedd8bea4
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-08 14:11:35 +0000

    group hovering for file

--- components/File/index.tsx
+++ components/File/index.tsx
@@ -18,14 +18,14 @@ import MoreIcon from "../Icons/MoreIcon";
 
 export default function CreateFile() {
   return (
-    <View width="100%" padding={6} className="">
+    <View width="100%" padding={6} className="group">
       {/* File component */}
       <View gap={4}>
         <View
           borderColor="neutral-faded"
           aspectRatio={1 / 1}
           borderRadius="small"
-          className="border-dashed transition ease-in-out duration-300 hover:bg-neutral-highlighted"
+          className="border-dashed transition ease-in-out duration-300 group-hover:bg-neutral-highlighted"
           justify="center"
           align="center"
           padding={{ xl: 16, l: 14, m: 10, s: 10 }}
@@ -34,7 +34,13 @@ export default function CreateFile() {
         </View>
         {/* Label */}
         <View align="center" paddingBottom={6}>
-          <Text variant="body-2" weight="medium" align="center" color="primary">
+          <Text
+            variant="body-2"
+            weight="medium"
+            align="center"
+            color="primary"
+            className="group-hover:opacity-90"
+          >
             Create New Document
           </Text>
         </View>
@@ -45,7 +51,7 @@ export default function CreateFile() {
 
 export function TemplateFile() {
   return (
-    <View width="100%" padding={6}>
+    <View width="100%" padding={6} className="group">
       {/* File component */}
       <View gap={4}>
         <View
@@ -56,37 +62,31 @@ export function TemplateFile() {
           align="center"
           position="relative"
           padding={{ xl: 16, l: 14, m: 10, s: 10 }}
-          className="transition ease-in-out duration-300 hover:bg-neutral-highlighted"
+          className="transition ease-in-out duration-300 group-hover:bg-neutral-highlighted "
         >
-          <View position="absolute" insetTop={2} insetEnd={2}>
-            <DropdownMenu>
-              <DropdownMenu.Trigger>
-                {(attributes) => (
-                  <Button
-                    rounded={true}
-                    elevated={true}
-                    icon={<MoreIcon />}
-                    color="white"
-                  ></Button>
-                )}
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Section>
-                  <DropdownMenu.Item>Action 1</DropdownMenu.Item>
-                  <DropdownMenu.Item>Action 2</DropdownMenu.Item>
-                </DropdownMenu.Section>
-                <DropdownMenu.Section>
-                  <DropdownMenu.Item>Action 3</DropdownMenu.Item>
-                  <DropdownMenu.Item>Action 4</DropdownMenu.Item>
-                </DropdownMenu.Section>
-              </DropdownMenu.Content>
-            </DropdownMenu>
+          <View
+            position="absolute"
+            insetTop={2}
+            insetEnd={2}
+            className="transition ease-in-out duration-300 opacity-0 group-hover:opacity-100"
+          >
+            <Button
+              rounded={true}
+              elevated={true}
+              icon={<MoreIcon />}
+              color="white"
+            ></Button>
           </View>
           <Template />
         </View>
         {/* Label */}
         <View align="center" paddingBottom={6} gap={1}>
-          <Text variant="body-2" weight="medium" align="center">
+          <Text
+            variant="body-2"
+            weight="medium"
+            align="center"
+            className="group-hover:text-neutral-faded"
+          >
             Financial Business Plan
           </Text>
           <Text variant="caption-1" align="center" color="neutral-faded">
