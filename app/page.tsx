4083a410b93c48571ab543eb5b25bf818b243490
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-09 12:09:16 +0000

    File doc navigation. draft

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import {
 import CreateFile from "@/components/File/index";
 import DocumentHeader from "@/components/DocumentHeader/index";
 import { TemplateFile, DocumentFile } from "@/components/File/index";
+import FileNavigation from "@/components/FileNavigation";
 
 export default function Home() {
   return (
@@ -24,7 +25,11 @@ export default function Home() {
     >
       <DocumentHeader />
       {/* File page section */}
-      <View width="100%"></View>
+      <View width="100%" paddingInline={6} paddingTop={16}>
+        <View.Item columns={2}>
+          <FileNavigation />
+        </View.Item>
+      </View>
 
       <View.Item columns={{ xl: 3, l: 3, m: 4, s: 6 }}>
         <CreateFile />
--- components/FileNavigation/index.tsx
+++ components/FileNavigation/index.tsx
@@ -13,13 +13,47 @@ import {
   useFormControl,
   FormControlProps,
   Divider,
+  Accordion,
+  AccordionProps,
+  MenuItem,
 } from "reshaped";
-import ArrowLeft from "../Icons/ArrowLeft";
-import DocsIcon from "../Icons/DocsIcon";
-import MoreIcon from "../Icons/MoreIcon";
-import PrintIcon from "../Icons/PrintIcon";
-import ShareIcon from "../Icons/ShareIcon";
+
+import PlusIcon from "../Icons/PlusIcon";
 
 export default function FileNavigation() {
-  return <View width="100%"></View>;
+  return (
+    <View width="100%">
+      {/* Headline with add button */}
+      <View
+        direction="row"
+        width="100%"
+        className="justify-between"
+        align="center"
+        paddingBottom={4}
+      >
+        <Text variant="body-2" weight="medium">
+          Sections
+        </Text>
+        <Button
+          size="small"
+          variant="outline"
+          rounded
+          icon={<PlusIcon />}
+        ></Button>
+      </View>
+
+      <Accordion defaultActive={false}>
+        <Accordion.Trigger>
+          <View borderRadius="small" overflow="hidden" paddingBottom={1}>
+            <MenuItem>Section 1</MenuItem>
+          </View>
+        </Accordion.Trigger>
+        <Accordion.Content>
+          <View>
+            <View backgroundColor="neutral-faded" height={10} />
+          </View>
+        </Accordion.Content>
+      </Accordion>
+    </View>
+  );
 }
