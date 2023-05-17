a44bce505181b70a4f7f71bc7bef0ee07e613d65
Aditya Vaishnav <firehudson@gmail.com>
2023-05-17 12:52:22 +0530

    Fix hydration issue

--- app/(editor)/documents/[id]/page.tsx
+++ app/(editor)/documents/[id]/page.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import FileNavigation from '@/components/FileNavigation';
-import Plate from '@/components/Plate';
 import { MyValue } from '@/components/Plate/interfaces/plateTypes';
+import dynamic from 'next/dynamic';
 import { useMemo, useState } from 'react';
 import { View } from 'reshaped';
+
+const Plate = dynamic(() => import('@/components/Plate'), { ssr: false });
+const FileNavigation = dynamic(() => import('@/components/FileNavigation'), {
+  ssr: false,
+});
 
 const DocumentPage = () => {
   const [document, setDocument] = useState<MyValue>([]);
 
   const sections = useMemo(() => {
     let section = '';
-    
+
     return document.reduce((prev: { [key: string]: string[] }, curr) => {
       const elementType = curr?.type;
       const text: string = curr?.children?.[0]?.text as string;
