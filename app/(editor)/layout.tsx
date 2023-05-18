63a6b916a6f2e5ed59758fbd630ef4f9b72806a4
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-18 13:38:02 +0000

    docs header

--- app/(editor)/layout.tsx
+++ app/(editor)/layout.tsx
@@ -45,93 +45,14 @@ export default function CreateFile({
 
   return (
     <View width="100%" position="relative">
-      <View
-        direction="row"
-        paddingInline={6}
-        paddingBlock={4}
-        position="sticky"
-        insetTop={0}
-        width="100%"
-        backgroundColor="page"
-        zIndex={2}
-      >
-        {/* Back Button */}
-        <View.Item columns={4} className="print:hidden">
-          <View justify="start" align="start">
-            <Button rounded size="medium" icon={<ArrowLeft />}></Button>
-          </View>
-        </View.Item>
-
-        {/* File name & Type */}
-        <View.Item columns={4} className="print:hidden">
-          <View align="center">
-            <View className="group">
-              <View
-                direction="row"
-                borderRadius="small"
-                justify="center"
-                gap={2}
-                paddingInline={2}
-                paddingBlock={2}
-                className="w-fit transition ease-in-out duration-300 group-hover:bg-neutral-highlighted"
-              >
-                <Icon svg={<DocsIcon />} size={6} />
-                <View>
-                  <CustomInput />
-                </View>
-              </View>
-            </View>
-          </View>
-        </View.Item>
-        <View.Item columns={4} className="print:hidden">
-          <View justify="end" align="center" gap={3} direction="row">
-            <Button
-              onClick={() => {
-                window.print();
-              }}
-              rounded
-              size="medium"
-              icon={<PrintIcon />}
-            >
-              Print
-            </Button>
-            <Button rounded size="medium" icon={<ShareIcon />}>
-              Share
-            </Button>
-            <DropdownMenu position="bottom-end">
-              <DropdownMenu.Trigger>
-                {(attributes) => (
-                  <Button
-                    rounded
-                    size="medium"
-                    variant="ghost"
-                    icon={<MoreIcon />}
-                    attributes={attributes}
-                  ></Button>
-                )}
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Section>
-                  <DropdownMenu.Item startSlot={<RenameIcon />}>
-                    Rename
-                  </DropdownMenu.Item>
-                  <DropdownMenu.Item startSlot={<DuplicateIcon />}>
-                    Duplicate
-                  </DropdownMenu.Item>
-                  <DropdownMenu.Item startSlot={<BinIcon />}>
-                    Delete
-                  </DropdownMenu.Item>
-                </DropdownMenu.Section>
-              </DropdownMenu.Content>
-            </DropdownMenu>
-          </View>
-        </View.Item>
-      </View>
-      <Divider />
-
-      <View className="flex flex-row py-x2 px-x4  border-b-[1px] border-neutral-faded bg-page">
+      <View className="flex flex-row py-x4 px-x4  border-b-[1px] border-neutral-faded bg-page">
         <View className="flex basis-4/12 ">
-          <Button rounded size="medium" icon={<ArrowLeft />}></Button>
+          <Button
+            width="100%"
+            rounded
+            size="medium"
+            icon={<ArrowLeft />}
+          ></Button>
         </View>
         <View className="flex basis-4/12 justify-center w-x2">
           {" "}
@@ -152,8 +73,7 @@ export default function CreateFile({
             </View>
           </View>
         </View>
-        <View className="flex basis-4/12 justify-end gap-x-x3">
-          {" "}
+        <View className="flex basis-4/12 justify-end !gap-x-x3">
           <Button
             onClick={() => {
               window.print();
