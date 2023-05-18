ee33f7be1d35b56dca75b1d671cd6957910b3c55
Micha Mailänder <micha@mailaender.me>
2023-05-18 13:06:54 +0000

    Updated Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/log-in", "/sign-up"],
+  publicRoutes: ["/sign-in", "/sign-up"],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL("/sign-in", req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  },
 });
 
 export const config = {
