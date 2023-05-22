8b668bf8ab82a22983548e2bde5f184d582a4dcf
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-22 15:23:14 +0000

    fixed file nav padding

--- components/FileNavigation/index.tsx
+++ components/FileNavigation/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import {
   Button,
   View,
@@ -13,10 +13,10 @@ import {
   Placeholder,
   DropdownMenu,
   DropdownMenuProps,
-} from 'reshaped';
+} from "reshaped";
 
-import PlusIcon from '../Icons/PlusIcon';
-import Section from '../Section';
+import PlusIcon from "../Icons/PlusIcon";
+import Section from "../Section";
 
 interface FileNavigationProps {
   sections: { [sectionName: string]: string[] };
@@ -24,31 +24,27 @@ interface FileNavigationProps {
 
 export default function FileNavigation({ sections }: FileNavigationProps) {
   return (
-    <View width='100%'>
+    <View width="100%">
       {/* Headline with add button */}
       <View
-        direction='row'
-        width='100%'
-        align='center'
+        direction="row"
+        width="100%"
+        align="center"
         paddingBottom={6}
         paddingStart={4}
-        paddingTop={4}
+        paddingTop={2}
       >
         <View.Item grow>
-          <Text
-            variant='body-3'
-            weight='medium'
-            color='neutral-faded'
-          >
+          <Text variant="body-3" weight="medium" color="neutral-faded">
             Sections
           </Text>
         </View.Item>
-        <DropdownMenu position='bottom'>
+        <DropdownMenu position="bottom">
           <DropdownMenu.Trigger>
             {(attributes) => (
               <Button
-                size='small'
-                variant='outline'
+                size="small"
+                variant="outline"
                 rounded
                 icon={<PlusIcon />}
                 attributes={attributes}
@@ -66,11 +62,7 @@ export default function FileNavigation({ sections }: FileNavigationProps) {
       {Object.keys(sections).map(
         (sectionName: string, sectionIndex: number) => {
           return (
-            <View
-              width='100%'
-              key={sectionIndex}
-              paddingEnd={2}
-            >
+            <View width="100%" key={sectionIndex} paddingEnd={2}>
               <Accordion defaultActive={false}>
                 <Accordion.Trigger>
                   <View paddingBottom={2}>
@@ -81,22 +73,15 @@ export default function FileNavigation({ sections }: FileNavigationProps) {
                   {/* List of subsection */}
                   {sections[sectionName].map(
                     (subsection: string, index: number) => (
-                      <View
-                        gap={1}
-                        key={index}
-                        paddingBottom={3}
-                      >
+                      <View gap={1} key={index} paddingBottom={3}>
                         <View
-                          direction='row'
-                          align='center'
+                          direction="row"
+                          align="center"
                           gap={3}
                           paddingStart={4}
-                          width='100%'
+                          width="100%"
                         >
-                          <View
-                            height={10}
-                            align='start'
-                          >
+                          <View height={10} align="start">
                             <Divider vertical />
                           </View>
                           {/* subsection list */}
