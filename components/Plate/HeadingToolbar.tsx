ed9cbaee507aa851b3a45781c79766a176cbdee2
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-26 11:42:29 +0000

    Added all styling options to Heading Toolbar

--- components/Plate/HeadingToolbar.tsx
+++ components/Plate/HeadingToolbar.tsx
@@ -21,6 +21,13 @@ import {
   ELEMENT_OL,
   ELEMENT_UL,
   ListToolbarButton,
+  BalloonToolbar,
+  BalloonToolbarProps,
+  ELEMENT_TABLE,
+  WithPartial,
+  LinkToolbarButton,
+  TComboboxItem,
+  HeadingToolbar,
 } from "@udecode/plate";
 import { FormatStrikethrough } from "@styled-icons/material/FormatStrikethrough";
 import { FormatUnderlined } from "@styled-icons/material/FormatUnderlined";
@@ -43,19 +50,80 @@ import StrikethroughIcon from "@/components/Icons/StrikethroughIcon";
 import LinkIcon from "@/components/Icons/LinkIcon";
 import { FormatListBulleted } from "@styled-icons/material/FormatListBulleted";
 import { FormatListNumbered } from "@styled-icons/material/FormatListNumbered";
-import { View } from "reshaped";
+import { View, Select } from "reshaped";
 import BulletlistIcon from "../Icons/BulletlistIcon";
 import OrderlistIcon from "../Icons/OrderlistIcon";
+import TextIcon from "@/components/Icons/TextIcon";
+import { TippyProps } from "@tippyjs/react";
+import { Transforms } from "slate";
+import { useMyPlateEditorRef } from "./interfaces/plateTypes";
 
 const tooltip = (content: string) => ({
   content,
 });
 
+export const markTooltip: TippyProps = {
+  arrow: true,
+  delay: 0,
+  duration: [200, 0],
+  hideOnClick: false,
+  // offset: [0, 17],
+  placement: "top",
+};
+
+const options = [
+  {
+    value: "h1",
+    label: "Heading 1",
+  },
+  {
+    value: "h2",
+    label: "Heading 2",
+  },
+  {
+    value: "h3",
+    label: "Heading 3",
+  },
+  {
+    value: "p",
+    label: "Paragraph",
+  },
+];
+
 export const BasicElementToolbarButtons = () => {
   const editor = usePlateEditorRef(useEventPlateId());
 
+  const arrow = false;
+  const theme = "light";
+
+  const boldTooltip: TippyProps = { content: "", ...markTooltip };
+  const italicTooltip: TippyProps = { content: "", ...markTooltip };
+  const underlineTooltip: TippyProps = {
+    content: "",
+    ...markTooltip,
+  };
+  const strikethroughTooltip: TippyProps = { content: "", ...markTooltip };
+  const linkTooltip: TippyProps = { content: "", ...markTooltip };
+
+  const handleOption = (selectedOption: any) => {
+    Transforms.setNodes(editor as any, { type: selectedOption.value } as any);
+  };
+
+  function tooltip(arg0: string) {
+    throw new Error("Function not implemented.");
+  }
+
   return (
-    <View direction="row" align="center">
+    <View direction="row" align="center" divided gap={1}>
+      <View width="160px" maxWidth="100%" paddingInline={2}>
+        <Select
+          onChange={handleOption}
+          options={options}
+          name="sort"
+          icon={<TextIcon />}
+          variant="headless"
+        />
+      </View>
       {/* <BlockToolbarButton
         tooltip={tooltip("Heading 1")}
         type={getPluginType(editor, ELEMENT_H1)}
@@ -72,89 +140,40 @@ export const BasicElementToolbarButtons = () => {
         icon={<Looks3 />}
       /> */}
 
-      {/* <BlockToolbarButton
-        tooltip={tooltip("Heading 4")}
-        type={getPluginType(editor, ELEMENT_H4)}
-        icon={<Looks4 />}
-      />
-      <BlockToolbarButton
-        tooltip={tooltip("Heading 5")}
-        type={getPluginType(editor, ELEMENT_H5)}
-        icon={<Looks5 />}
-      />
-      <BlockToolbarButton
-        tooltip={tooltip("Heading 6")}
-        type={getPluginType(editor, ELEMENT_H6)}
-        icon={<Looks6 />}
-      /> */}
-
-      {/* <MarkToolbarButton
-        tooltip={tooltip('')}
-        type={getPluginType(editor, MARK_BOLD)}
-        icon={
-          <TextStyle
-            label='Bold'
-            icon={<BoldIcon />}
-          />
-        }
-      />
-      <MarkToolbarButton
-        tooltip={tooltip('')}
-        type={getPluginType(editor, MARK_ITALIC)}
-        icon={
-          <TextStyle
-            label='Italic'
-            icon={<ItalicIcon />}
-          />
-        }
-      /> */}
-      {/* <MarkToolbarButton
-        tooltip={tooltip('')}
-        type={getPluginType(editor, MARK_UNDERLINE)}
-        icon={
-          <TextStyle
-            label='Underline'
-            icon={<UnderlineIcon />}
-          />
-        }
-      />
-      <MarkToolbarButton
-        tooltip={tooltip('')}
-        type={getPluginType(editor, MARK_STRIKETHROUGH)}
-        icon={
-          <TextStyle
-            label='Strikethrough'
-            icon={<StrikethroughIcon />}
-          />
-        }
-      /> */}
-
-      {/* <MarkToolbarButton
-        tooltip={tooltip("Superscript (⌘+,)")}
-        type={getPluginType(editor, MARK_SUPERSCRIPT)}
-        clear={getPluginType(editor, MARK_SUBSCRIPT)}
-        icon={<Superscript />}
-      />
-      <MarkToolbarButton
-        tooltip={tooltip("Subscript (⌘+.)")}
-        type={getPluginType(editor, MARK_SUBSCRIPT)}
-        clear={getPluginType(editor, MARK_SUPERSCRIPT)}
-        icon={<Subscript />}
-      />
-      <BlockToolbarButton
-        tooltip={tooltip("Block Quote (⌘+⇧+.)")}
-        type={getPluginType(editor, ELEMENT_BLOCKQUOTE)}
-        icon={<FormatQuote />}
-      /> */}
-
-      <ListToolbarButton
-        type={getPluginType(editor, ELEMENT_UL)}
-        icon={<TextStyle label="Bullet List" icon={<BulletlistIcon />} />}
-      />
-      <ListToolbarButton
-        type={getPluginType(editor, ELEMENT_OL)}
-        icon={<TextStyle label="Ordered List" icon={<OrderlistIcon />} />}
-      />
+      <View direction="row" align="center" gap={1}>
+        <MarkToolbarButton
+          type={getPluginType(editor, MARK_BOLD)}
+          icon={<TextStyle label="Bold (⌘+B)" icon={<BoldIcon />} />}
+          actionHandler="onMouseDown"
+        />
+        <MarkToolbarButton
+          type={getPluginType(editor, MARK_ITALIC)}
+          icon={<TextStyle label="Italic (⌘+I)" icon={<ItalicIcon />} />}
+          actionHandler="onMouseDown"
+        />
+        <MarkToolbarButton
+          type={getPluginType(editor, MARK_UNDERLINE)}
+          icon={<TextStyle label="Underline (⌘+U)" icon={<UnderlineIcon />} />}
+          actionHandler="onMouseDown"
+        />
+        <MarkToolbarButton
+          type={getPluginType(editor, MARK_STRIKETHROUGH)}
+          icon={
+            <TextStyle label="Strikethrough" icon={<StrikethroughIcon />} />
+          }
+          actionHandler="onMouseDown"
+        />
+      </View>
+      <View direction="row" align="center">
+        <ListToolbarButton
+          type={getPluginType(editor, ELEMENT_UL)}
+          icon={<TextStyle label="Bullet List" icon={<BulletlistIcon />} />}
+        />
+        <ListToolbarButton
+          type={getPluginType(editor, ELEMENT_OL)}
+          icon={<TextStyle label="Ordered List" icon={<OrderlistIcon />} />}
+        />
+      </View>
     </View>
   );
 };
--- components/Plate/MarkBalloonToolbar.tsx
+++ components/Plate/MarkBalloonToolbar.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { FormatBold } from '@styled-icons/material/FormatBold';
-import { FormatItalic } from '@styled-icons/material/FormatItalic';
-import { FormatUnderlined } from '@styled-icons/material/FormatUnderlined';
-import { TippyProps } from '@tippyjs/react';
+import React from "react";
+import { FormatBold } from "@styled-icons/material/FormatBold";
+import { FormatItalic } from "@styled-icons/material/FormatItalic";
+import { FormatUnderlined } from "@styled-icons/material/FormatUnderlined";
+import { TippyProps } from "@tippyjs/react";
 import {
   BalloonToolbar,
   BalloonToolbarProps,
   ELEMENT_LINK,
   getPluginType,
+  useEventPlateId,
+  usePlateEditorRef,
   ELEMENT_TABLE,
   MARK_BOLD,
   MARK_ITALIC,
@@ -20,20 +22,20 @@ import {
   ELEMENT_UL,
   ELEMENT_OL,
   TComboboxItem,
-} from '@udecode/plate';
-import { useMyPlateEditorRef } from './interfaces/plateTypes';
-import { Select, View, ViewProps, SelectProps } from 'reshaped';
-import TextStyle from '@/components/TextStyle';
-import ItalicIcon from '@/components/Icons/ItalicIcon';
-import UnderlineIcon from '@/components/Icons/UnderlineIcon';
-import BoldIcon from '@/components/Icons/BoldIcon';
-import StrikethroughIcon from '@/components/Icons/StrikethroughIcon';
-import LinkIcon from '@/components/Icons/LinkIcon';
-import TextIcon from '@/components/Icons/TextIcon';
-import TableIcon from '@/components/Icons/TableIcon';
-import BulletlistIcon from '../Icons/BulletlistIcon';
-import OrderlistIcon from '../Icons/OrderlistIcon';
-import { Transforms } from 'slate';
+} from "@udecode/plate";
+import { useMyPlateEditorRef } from "./interfaces/plateTypes";
+import { Select, View, ViewProps, SelectProps } from "reshaped";
+import TextStyle from "@/components/TextStyle";
+import ItalicIcon from "@/components/Icons/ItalicIcon";
+import UnderlineIcon from "@/components/Icons/UnderlineIcon";
+import BoldIcon from "@/components/Icons/BoldIcon";
+import StrikethroughIcon from "@/components/Icons/StrikethroughIcon";
+import LinkIcon from "@/components/Icons/LinkIcon";
+import TextIcon from "@/components/Icons/TextIcon";
+import TableIcon from "@/components/Icons/TableIcon";
+import BulletlistIcon from "../Icons/BulletlistIcon";
+import OrderlistIcon from "../Icons/OrderlistIcon";
+import { Transforms } from "slate";
 
 export const markTooltip: TippyProps = {
   arrow: true,
@@ -41,111 +43,111 @@ export const markTooltip: TippyProps = {
   duration: [200, 0],
   hideOnClick: false,
   // offset: [0, 17],
-  placement: 'top',
+  placement: "top",
 };
 
 const options = [
   {
-    value: 'h1',
-    label: 'Heading 1',
+    value: "h1",
+    label: "Heading 1",
   },
   {
-    value: 'h2',
-    label: 'Heading 2',
+    value: "h2",
+    label: "Heading 2",
   },
   {
-    value: 'h3',
-    label: 'Heading 3',
+    value: "h3",
+    label: "Heading 3",
   },
   {
-    value: 'p',
-    label: 'Paragraph',
+    value: "p",
+    label: "Paragraph",
   },
 ];
 
 export const MarkBalloonToolbar = (
-  props: WithPartial<BalloonToolbarProps, 'children'>
+  props: WithPartial<BalloonToolbarProps, "children">
 ) => {
   const { children, ...balloonToolbarProps } = props;
 
   const editor = useMyPlateEditorRef();
 
   const arrow = false;
-  const theme = 'light';
+  const theme = "light";
 
-  const boldTooltip: TippyProps = { content: '', ...markTooltip };
-  const italicTooltip: TippyProps = { content: '', ...markTooltip };
+  const boldTooltip: TippyProps = { content: "", ...markTooltip };
+  const italicTooltip: TippyProps = { content: "", ...markTooltip };
   const underlineTooltip: TippyProps = {
-    content: '',
+    content: "",
     ...markTooltip,
   };
-  const strikethroughTooltip: TippyProps = { content: '', ...markTooltip };
-  const linkTooltip: TippyProps = { content: '', ...markTooltip };
+  const strikethroughTooltip: TippyProps = { content: "", ...markTooltip };
+  const linkTooltip: TippyProps = { content: "", ...markTooltip };
 
   const handleOption = (selectedOption: any) => {
     Transforms.setNodes(editor as any, { type: selectedOption.value } as any);
   };
 
   function tooltip(arg0: string) {
-    throw new Error('Function not implemented.');
+    throw new Error("Function not implemented.");
   }
 
   return (
     <BalloonToolbar theme={theme} arrow={arrow} {...balloonToolbarProps}>
-      <View direction='row' align='center' divided gap={1}>
-        <View width='160px' maxWidth='100%' paddingInline={2}>
+      <View direction="row" align="center" divided gap={1}>
+        <View width="160px" maxWidth="100%" paddingInline={2}>
           <Select
             onChange={handleOption}
             options={options}
-            name='sort'
+            name="sort"
             icon={<TextIcon />}
-            variant='headless'
+            variant="headless"
           />
         </View>
-        <View direction='row' align='center' gap={1}>
+        <View direction="row" align="center" gap={1}>
           <MarkToolbarButton
             type={getPluginType(editor, MARK_BOLD)}
-            icon={<TextStyle label='Bold (⌘+B)' icon={<BoldIcon />} />}
+            icon={<TextStyle label="Bold (⌘+B)" icon={<BoldIcon />} />}
             tooltip={boldTooltip}
-            actionHandler='onMouseDown'
+            actionHandler="onMouseDown"
           />
           <MarkToolbarButton
             type={getPluginType(editor, MARK_ITALIC)}
-            icon={<TextStyle label='Italic (⌘+I)' icon={<ItalicIcon />} />}
+            icon={<TextStyle label="Italic (⌘+I)" icon={<ItalicIcon />} />}
             tooltip={italicTooltip}
-            actionHandler='onMouseDown'
+            actionHandler="onMouseDown"
           />
           <MarkToolbarButton
             type={getPluginType(editor, MARK_UNDERLINE)}
             icon={
-              <TextStyle label='Underline (⌘+U)' icon={<UnderlineIcon />} />
+              <TextStyle label="Underline (⌘+U)" icon={<UnderlineIcon />} />
             }
             tooltip={underlineTooltip}
-            actionHandler='onMouseDown'
+            actionHandler="onMouseDown"
           />
           <MarkToolbarButton
             tooltip={strikethroughTooltip}
             type={getPluginType(editor, MARK_STRIKETHROUGH)}
             icon={
-              <TextStyle label='Strikethrough' icon={<StrikethroughIcon />} />
+              <TextStyle label="Strikethrough" icon={<StrikethroughIcon />} />
             }
-            actionHandler='onMouseDown'
+            actionHandler="onMouseDown"
           />
         </View>
-        <View direction='row' align='center'>
+        <View direction="row" align="center">
           <ListToolbarButton
             type={getPluginType(editor, ELEMENT_UL)}
-            icon={<TextStyle label='Bullet List' icon={<BulletlistIcon />} />}
+            icon={<TextStyle label="Bullet List" icon={<BulletlistIcon />} />}
           />
           <ListToolbarButton
             type={getPluginType(editor, ELEMENT_OL)}
-            icon={<TextStyle label='Ordered List' icon={<OrderlistIcon />} />}
+            icon={<TextStyle label="Ordered List" icon={<OrderlistIcon />} />}
           />
         </View>
-        <View direction='row' align='center' paddingEnd={1}>
-          <View direction='row' align='center'>
+        <View direction="row" align="center" paddingEnd={1}>
+          <View direction="row" align="center">
             <LinkToolbarButton
-              icon={<TextStyle label='Link' icon={<LinkIcon />} />}
+              icon={<TextStyle label="Link" icon={<LinkIcon />} />}
             />
           </View>
         </View>
