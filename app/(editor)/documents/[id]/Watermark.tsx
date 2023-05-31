5722ba084116184cf13596d171e3270aebe9f0b3
bohdank1995 <bohdan.karpliak@dechea.com>
2023-05-31 12:29:02 +0000

    Watermark

--- app/(editor)/documents/[id]/Watermark.tsx
+++ app/(editor)/documents/[id]/Watermark.tsx
@@ -3,27 +3,31 @@ import { View, Text, Icon, Link, Divider } from "reshaped";
 import React from "react";
 import Image from "next/image";
 import ExternalLink from "@/components/Icons/ExternalLink";
+import XGroup from "@/components/Icons/XGroup";
 
 export default function Watermark() {
   return (
-    <View
-      className="w-full border-t-[2px] border-neutral-faded "
-      justify="end"
-      paddingInline={20}
-    >
+    <View justify="end" paddingStart={20}>
       <View
+        className="w-full border border-neutral-faded "
         direction="row"
         align="center"
-        gap={1}
-        paddingTop={4}
-        width="900px"
-        maxWidth="100%"
+        gap={3}
+        paddingStart={3}
+        paddingBlock={3}
+        paddingEnd={6}
+        borderRadius="medium"
       >
-        <Icon svg={<ExternalLink />} size={8} />
-        <Link color="inherit" variant="plain" href="https://www.instagram.com/">
-          Create your Business Plan with Compademy
-        </Link>
-        <Link icon={<ExternalLink />} />
+        <View direction="row" align="center" gap={3}>
+          <Icon svg={<XGroup />} size={8} className="opacity-40" />
+          <Link
+            color="inherit"
+            variant="plain"
+            href="https://www.instagram.com/"
+          >
+            Create your Business Plan with Compademy
+          </Link>
+        </View>
       </View>
     </View>
   );
