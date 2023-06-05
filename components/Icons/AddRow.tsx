1b10d0ac8860605c6620eb483b21d427cb10c831
Aditya Vaishnav <firehudson@gmail.com>
2023-06-05 08:10:27 +0000

    Replace clip-path with clipPath

--- components/Icons/AddRow.tsx
+++ components/Icons/AddRow.tsx
@@ -1,49 +1,49 @@
 const AddRow = () => {
   return (
     <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      width='32'
+      height='32'
+      viewBox='0 0 32 32'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
     >
-      <g clip-path="url(#clip0_540_15408)">
-        <mask id="path-1-inside-1_540_15408" fill="white">
-          <rect x="1" y="-6.21429" width="30" height="12.8571" rx="2" />
+      <g clipPath='url(#clip0_540_15408)'>
+        <mask id='path-1-inside-1_540_15408' fill='white'>
+          <rect x='1' y='-6.21429' width='30' height='12.8571' rx='2' />
         </mask>
         <rect
-          x="1"
-          y="-6.21429"
-          width="30"
-          height="12.8571"
-          rx="2"
-          stroke="#C7CDDB"
-          stroke-width="6"
-          mask="url(#path-1-inside-1_540_15408)"
+          x='1'
+          y='-6.21429'
+          width='30'
+          height='12.8571'
+          rx='2'
+          stroke='#C7CDDB'
+          stroke-width='6'
+          mask='url(#path-1-inside-1_540_15408)'
         />
-        <mask id="path-2-inside-2_540_15408" fill="white">
-          <rect x="1" y="8.78571" width="30" height="21.4286" rx="2" />
+        <mask id='path-2-inside-2_540_15408' fill='white'>
+          <rect x='1' y='8.78571' width='30' height='21.4286' rx='2' />
         </mask>
         <rect
-          x="1"
-          y="8.78571"
-          width="30"
-          height="21.4286"
-          rx="2"
-          stroke="#14171F"
-          stroke-width="6"
-          mask="url(#path-2-inside-2_540_15408)"
+          x='1'
+          y='8.78571'
+          width='30'
+          height='21.4286'
+          rx='2'
+          stroke='#14171F'
+          stroke-width='6'
+          mask='url(#path-2-inside-2_540_15408)'
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M15.2501 20.7499V24.64C15.2501 24.8388 15.4113 25 15.6101 25H17.3899C17.5887 25 17.7499 24.8388 17.7499 24.64V20.7499H21.64C21.8388 20.7499 22 20.5887 22 20.3899V18.6101C22 18.4113 21.8388 18.2501 21.64 18.2501H17.7499V14.36C17.7499 14.1612 17.5887 14 17.3899 14H15.6101C15.4113 14 15.2501 14.1612 15.2501 14.36V18.2501H11.36C11.1612 18.2501 11 18.4113 11 18.6101V20.3899C11 20.5887 11.1612 20.7499 11.36 20.7499H15.2501Z"
-          fill="black"
+          fill-rule='evenodd'
+          clip-rule='evenodd'
+          d='M15.2501 20.7499V24.64C15.2501 24.8388 15.4113 25 15.6101 25H17.3899C17.5887 25 17.7499 24.8388 17.7499 24.64V20.7499H21.64C21.8388 20.7499 22 20.5887 22 20.3899V18.6101C22 18.4113 21.8388 18.2501 21.64 18.2501H17.7499V14.36C17.7499 14.1612 17.5887 14 17.3899 14H15.6101C15.4113 14 15.2501 14.1612 15.2501 14.36V18.2501H11.36C11.1612 18.2501 11 18.4113 11 18.6101V20.3899C11 20.5887 11.1612 20.7499 11.36 20.7499H15.2501Z'
+          fill='black'
         />
       </g>
       <defs>
-        <clipPath id="clip0_540_15408">
-          <rect width="32" height="32" fill="white" />
+        <clipPath id='clip0_540_15408'>
+          <rect width='32' height='32' fill='white' />
         </clipPath>
       </defs>
     </svg>
