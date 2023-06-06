b43af4e982ecca6d9cdff6b3fa314d9a6ed4aa8e
Aditya Vaishnav <firehudson@gmail.com>
2023-06-06 11:11:56 +0000

    Remove "Create an account" button

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,26 +1,17 @@
-"use client";
-import CreateFile, { DocumentFile } from "@/components/File";
+'use client';
+import CreateFile, { DocumentFile } from '@/components/File';
 
-import { View, useToast, Button, Tabs } from "reshaped";
+import { View, useToast, Button, Tabs } from 'reshaped';
 
 const page = () => {
   const toast = useToast();
   return (
-    <View direction="row">
+    <View direction='row'>
       <View.Item columns={{ xl: 2, l: 3, m: 4, s: 6 }}>
         <CreateFile />
       </View.Item>
       <View.Item columns={{ xl: 2, l: 3, m: 4, s: 6 }}>
         <DocumentFile />
-      </View.Item>
-      <View.Item columns={{ xl: 2, l: 3, m: 4, s: 6 }}>
-        <Button
-          onClick={() =>
-            toast.show({ text: "Account created", position: "bottom" })
-          }
-        >
-          Create an account
-        </Button>
       </View.Item>
     </View>
   );
