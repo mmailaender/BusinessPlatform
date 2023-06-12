390ca65887b033b2575bba3c827bc076e9ed7223
Aditya Vaishnav <firehudson@gmail.com>
2023-06-12 10:31:48 +0000

    Colocate tabs for dashboard layout

---
+++ app/(dashboard)/tabs.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { View, Tabs as ReshapedTabs } from 'reshaped';
+import Link from 'next/link';
+
+import Docs from '@/components/Icons/DocsIcon';
+import Template from '@/components/Icons/TemplateIcon';
+import { usePathname } from 'next/navigation';
+
+export function Tabs() {
+  const path = usePathname();
+  return (
+    <View paddingBlock={0} paddingInline={6}>
+      <ReshapedTabs
+        variant='borderless'
+        defaultValue={path.includes('templates') ? '1' : '0'}
+      >
+        <ReshapedTabs.List>
+          <ReshapedTabs.Item value='0' icon={<Docs />}>
+            <Link href='/'>Documents</Link>
+          </ReshapedTabs.Item>
+          <ReshapedTabs.Item value='1' icon={<Template />}>
+            <Link href='/templates'>Templates</Link>
+          </ReshapedTabs.Item>
+        </ReshapedTabs.List>
+      </ReshapedTabs>
+    </View>
+  );
+}
