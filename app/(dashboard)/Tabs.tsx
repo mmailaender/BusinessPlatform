8cae72823673e0595a126e9d6391260585b8b09d
Aditya Vaishnav <firehudson@gmail.com>
2023-07-11 11:26:33 +0000

    Fixed tabs

--- app/(dashboard)/Tabs.tsx
+++ app/(dashboard)/Tabs.tsx
@@ -1,33 +1,39 @@
 'use client';
 
-import { View, Tabs as ReshapedTabs } from 'reshaped';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import Docs from '@/components/Icons/DocsIcon';
-import Template from '@/components/Icons/TemplateIcon';
+
+const tabs = [
+  { name: 'Documents', href: '/' },
+  { name: 'Templates', href: '/templates' },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 export function Tabs() {
   const path = usePathname();
-
   return (
-    <View paddingBlock={0} paddingInline={6}>
-      <ReshapedTabs
-        variant='borderless'
-        defaultValue={path.includes('templates') ? '1' : '0'}
-      >
-        <ReshapedTabs.List>
-          <ReshapedTabs.Item value='0' icon={<Docs />}>
-            <Link href='/' passHref>
-              Documents
-            </Link>
-          </ReshapedTabs.Item>
-          <ReshapedTabs.Item value='1' icon={<Template />}>
-            <Link href='/templates' passHref>
-              Templates
-            </Link>
-          </ReshapedTabs.Item>
-        </ReshapedTabs.List>
-      </ReshapedTabs>
-    </View>
+    <div className='border-b border-neutral'>
+      <nav className='ml-x6 flex space-x-x8' aria-label='Tabs'>
+        {tabs.map((tab) => (
+          <Link
+            key={tab.name}
+            href={tab.href}
+            style={{ textDecoration: 'none' }}
+            className={classNames(
+              tab.href === path
+                ? 'border-primary text-primary'
+                : 'border-white text-neutral-faded hover:border-neutral hover:text-neutral',
+              'whitespace-nowrap border-b-2 py-x4 px-x1 text-sm font-medium'
+            )}
+            aria-current={tab.href === path ? 'page' : undefined}
+          >
+            {tab.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
