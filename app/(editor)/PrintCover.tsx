bd52320cd95a7ac86453b348322c77a829a9ef9c
Aditya Vaishnav <firehudson@gmail.com>
2023-07-13 10:50:23 +0000

    Dynamic name and image caption in print fix

--- app/(editor)/PrintCover.tsx
+++ app/(editor)/PrintCover.tsx
@@ -1,40 +1,57 @@
-"use client";
+'use client';
 
-import { View, Text } from "reshaped";
-import React from "react";
-import Image from "next/image";
+import { View, Text } from 'reshaped';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useParams, usePathname } from 'next/navigation';
+import { useQuery } from 'fqlx-client';
+import { Query } from '@/fqlx-generated/typedefs';
 
 export default function PrintCover() {
+  const [type, setType] = useState<'Template' | 'Document'>('Template');
+  const param = useParams();
+  const path = usePathname();
+  const query = useQuery<Query>();
+  const name = query[type].byId(param.id).project({ name: true }).exec().name;
+
+  useEffect(() => {
+    if (path.includes('templates')) {
+      setType('Template');
+    } else {
+      setType('Document');
+    }
+  }, [path]);
+
   return (
     <View
-      className="w-full "
+      className='w-full'
       height={376}
-      justify="end"
+      justify='end'
       paddingBottom={64}
       paddingInline={20}
     >
       <View width={80} height={80}>
         <Image
-          alt="x-group logo"
-          src="/X-GROUP-logo.png"
+          alt='x-group logo'
+          src='/X-GROUP-logo.png'
           width={300}
           height={103}
         />
       </View>
       <View>
         <View paddingBottom={24}>
-          <Text variant="title-3"> Business Plan Title Example</Text>
+          <Text variant='title-3'>{name}</Text>
         </View>
         <View gap={16}>
           <View gap={3}>
-            <Text variant="body-1">FOR COMPANY</Text>
-            <Text variant="title-6" weight="regular">
+            <Text variant='body-1'>FOR COMPANY</Text>
+            <Text variant='title-6' weight='regular'>
               Musterfirma GmbH
             </Text>
           </View>
           <View gap={3}>
-            <Text variant="body-1">CONSULTANT</Text>
-            <Text variant="title-6" weight="regular">
+            <Text variant='body-1'>CONSULTANT</Text>
+            <Text variant='title-6' weight='regular'>
               X-Group GmbH
             </Text>
           </View>
